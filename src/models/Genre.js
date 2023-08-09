c0c3c284bfe6f859da281c8fe2166e7872123c90
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-08-09 00:19:43 -0300

    Genre model

--- src/models/Genre.js
+++ src/models/Genre.js
@@ -11,10 +11,6 @@ module.exports = (sequelize)  => {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-      validate:{
-        is: /^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$/,// Nombre debe ser una sola palabra sin números ni símbolos
-        len:[3,20] //El nombre de la category debe tener mínimo 3 caracteres y máximo 20
-      },
     }
   },{
     timestamps:false,
