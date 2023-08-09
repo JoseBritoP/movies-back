bef8e77bd16de962a8d9178f5766b80bd5788321
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-08-09 08:34:27 -0300

    getMovieById

--- src/controllers/movies/05 - getMovieByID.js
+++ src/controllers/movies/05 - getMovieByID.js
@@ -1,9 +1,19 @@
 const { Movie, Genre } = require('../../db');
 
-const getMovieByID = (id) => {
-  // const movie = movies.find((movie) => movie.id === +id)
-  // if(!movie) throw Error(`No se encontró la película de id: ${id}`);
-  // return movie;
+const getMovieByID = async (id) => {
+  
+  if(!Number(id)) throw Error(`El id de la película debe ser númerico`)
+  const movie = await Movie.findByPk(id,{
+    include:{
+      model:Genre,
+      attributes:["id","name"],
+      through: { attributes: [] },
+    },
+  });
+
+  if(!movie) throw Error(`No existe la película de id: ${id}`)
+
+  return movie;
 };
 
 module.exports = getMovieByID;
