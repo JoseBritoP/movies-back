01700bf472e51f1f4f0443b9705ce6006c0fb129
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-08-09 12:32:18 -0300

    rutas no implementadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,4 +14,17 @@ const router = Router();
 router.use('/movies',moviesRouter)
 router.use('/genres',genresRouter);
 
+// Rutas no implementadas:
+router.use((req, res, next) => {
+  const error = new Error(`La ruta ${req.originalUrl} con el método ${req.method} no está implementada`);
+  error.status = 404;
+  next(error);
+});
+
+router.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    message: error.message || 'Error interno del servidor'
+  });
+});
+
 module.exports = router;
