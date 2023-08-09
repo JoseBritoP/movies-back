58d6ff744e9acc2e956f71e3dce071f4a32709ff
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-08-09 13:14:26 -0300

    Cors config

--- src/index.js
+++ src/index.js
@@ -10,9 +10,24 @@ const mainRouter = require('./routes/index');
 // Server
 
 const server = express();
+server.disable('x-powered-by')
 
 server.use(morgan('dev'));
-server.use(cors());
+server.use(cors({
+  origin: (origin,callback)=>{
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:3000'
+    ]
+    if(ACCEPTED_ORIGINS.includes(origin)){
+      return callback(null,true)
+    }
+    if(!origin){
+      return callback(null,true)
+    }
+
+    return callback(new Error('Not allowed by Cors'));
+  }
+}));
 server.use(express.json());
 
 // Conexión
